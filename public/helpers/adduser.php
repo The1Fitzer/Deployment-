8fca94227ac369cc1dc352c28fb55de180a65fd6
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-12 17:06:12 +0000

    facebook helper scripts added to login

--- public/helpers/adduser.php
+++ public/helpers/adduser.php
@@ -15,7 +15,9 @@
 		$check = "select * from $db_name.users where id = $user_id";
 		$result = $mysqli->query($check);
 		
-		if($mysqli->affected_rows < 1){
+		if($result){
+			//do nothing
+		}else{
 			$insert_user = "insert into $db_name.users(id,name,first_name,second_name,gender,locale,access_token) values($user_id, "$user_name","$first_name","$second_name","$gender","$locale","$access")";
 			$result = $mysqli->query($insert_user);
 		}
