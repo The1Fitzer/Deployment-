6fd27739a3d8e3ce2f75f9a7c0ed085bbe93e262
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 17:32:51 +0000

    new stuff

---
+++ public/helpers/assets/js/newnewnew.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$("#registerblock").hide();
+
+	$("#register").click(function(){
+		$("#registerblock").toggle();
+	});
+
+	$("#registerform").submit(function(e){
+		var username = $("#rusername").val();
+		var password = $("#rpassword").val();
+		var confpass = $("#confimpass").val();
+		console.log(password);
+		console.log(password);
+		if(username == ""){
+			$.jGrowl("The username field is blank!");
+			event.preventDefault();
+		}	
+		if(password === confpass){
+			$.ajax({
+				url: 'helpers/adduser.php',
+				type: 'POST',
+				data: {username:username,password:password},
+				success:function(data){
+					if(data === 1){
+						$.jGrowl('Registration Complete');
+						$("#registerblock").hide();
+					}else{
+						$.jGrowl('Registration not successful');
+					}
+					}
+			});
+		}
+	});	
+});
