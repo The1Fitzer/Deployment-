04d5a79c1ab1988876af40cc4c4f351237587bd8
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 18:44:54 +0000

    login

---
+++ public/helpers/login.php
@@ -0,0 +1,29 @@
+<?php
+	include('config.php');
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(isset($username)){
+		login($username, $password);
+	}else{
+		header("Location: http://deployment-project.orchestra.io/?s=2";
+	}
+
+	function login($username, $password){
+		global $db_name, $mysqli;
+		//check for user
+		$check = "select * from $db_name.users where username = '$username'";
+		$result = $mysqli->query($check) or die($mysqli->error);
+
+		if($result->affected_rows < 1){
+			header('Location: http://deployment-project.orchestra.io/?s=3');
+		}else{
+			$id = $result->fetch_object()->id;
+			$time = time() + 3600;
+			setcookie(username, $username, $time);
+			setcookie(id, $is, $time);
+
+			header('Location: http://deployment-project.orchestra.io/?s=4');
+		}
+	}
+?>
