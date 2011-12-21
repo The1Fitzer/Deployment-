6ecec3bae9cdcfa34c268c1c8f35e6781d9d5f8f
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 20:54:15 +0000

    youtube stuff

--- public/helpers/getvideos.php
+++ public/helpers/getvideos.php
@@ -1,11 +1,12 @@
 <?php
 	include('config.php');
-
+	$yt_array = array();
 	$query = "select * from $db_name.videos";
 
 	$result = $mysqli->query($query) or die($mysqli->error);
 
 	while($row = $result->fetch_object()){
-		echo $row->yt_id;
+		$yt_array[] = $row->yt_id;
 	}
+	print_r($yt_array);
 ?>
