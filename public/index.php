6f9b3b523c0bb48ddd8122f198e43707dda985a8
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 18:12:38 +0000

    new

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 		$full_query_string = $_SERVER['QUERY_STRING'];
 	}
 	$string = explode("=",$full_query_string);
-	echo $string;
+	print_r($string);
 	if($full_query_string == 1){
 		echo '<div style="top:50%; width:50%; background-color: green;">Registration Successfull</div>';
 	}
