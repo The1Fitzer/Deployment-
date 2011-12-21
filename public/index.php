5c2e7d8b80750f3d1ee49510edd14f3af7b84367
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 15:52:23 +0000

    bug fixes

--- public/index.php
+++ public/index.php
@@ -3,18 +3,17 @@
 	define('YOUR_APP_ID', '235024413234041');
 	define('YOUR_APP_SECRET','d78a09e226c896d919d1de00c4b1cce3');
 	
-	function get_facebook_cookie($app_id, $app_secret){
+	function get_facebook_cookie($app_id, $app_secret) {
 		$args = array();
-		parse_str(trim($_COOKIE['fbs_'.$app_id], '\\'), $args);
+		parse_str(trim($_COOKIE['fbs_' . $app_id], '\\"'), $args);
 		ksort($args);
 		$payload = '';
-		foreach ($args as $key => $value){
-			if($key != 'sig'){
-				$payload .= $key. '='.$value;
+		foreach ($args as $key => $value) {
+			if ($key != 'sig') {
+				$payload .= $key . '=' . $value;
 			}
 		}
-
-		if(md5($payload . $app_secret) != $args['sig']){
+		if (md5($payload . $app_secret) != $args['sig']) {
 			return null;
 		}
 		return $args;
