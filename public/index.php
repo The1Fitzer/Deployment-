73a9787ec93ec2634694ca806676a67f4382cd48
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 14:30:21 +0000

    facebook push

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@
 		var js, id = 'facebook-jssdk'; if(d.getElementById(id)) {return;}
 		js = d.createElement('script'); js.id = id; js.async = true;
 		js.src = "connect.facebook.net/en_US/all.js";
-		d.getElementsByTagName('head')[0].appendChile(js);
+		d.getElementsByTagName('head')[0].appendChild(js);
 	}(document));
 </script>
 
