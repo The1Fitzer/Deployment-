da1094aa139cd895495ac87687fc5ddf688a2db0
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 14:34:40 +0000

    facebook sdk and bug fixes

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@
 	(function(d){
 		var js, id = 'facebook-jssdk'; if(d.getElementById(id)) {return;}
 		js = d.createElement('script'); js.id = id; js.async = true;
-		js.src = "connect.facebook.net/en_US/all.js";
+		js.src = "//connect.facebook.net/en_US/all.js";
 		d.getElementsByTagName('head')[0].appendChild(js);
 	}(document));
 </script>
