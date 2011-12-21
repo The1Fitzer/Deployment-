726fce6e9ddafefb41f13d9e642c96a3ace5044c
Sam Cogan <cogansam@gmail.com>
2011-12-21 18:22:01 +0000

    jquery slide

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 	$string = explode("=",$full_query_string);
 	
 	if($string[1] == 1){
-		echo '<div style="top:50%; width:50%; background-color: green;">Registration Successfull</div>';
+		echo '<div style="margin:0 auto; top:200px; background-color: green;">Registration Successfull</div>';
 	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -13,6 +13,27 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
+
+<!-- Begin Stylesheets -->
+		<link rel="stylesheet" href="stylesheets/reset.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="stylesheets/coda-slider-2.0.css" type="text/css" media="screen" />
+	<!-- End Stylesheets -->
+	
+	<!-- Begin JavaScript -->
+		<script type="text/javascript" src="javascripts/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="javascripts/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="javascripts/jquery.coda-slider-2.0.js"></script>
+		 <script type="text/javascript">
+			$().ready(function() {
+				$('#coda-slider-1').codaSlider();
+			});
+		 </script>
+	<!-- End JavaScript -->
+
+
+
+
+
 <link rel="stylesheet" href="style.css" type="text/css" />
 <title>College Life</title>
 <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
@@ -41,11 +62,29 @@
 <div id="wrapper" style='background-color: white !important;'>
 <div id="menu"><ul><li>Link1</li><li>Link2</li><li>Link3</li><li>Link4</li><li>Link5</li><li>Link6</li></div>
 <div id="content">
-<br /><br /><br /><center>
-<table width="100%" height="500px" border="0">
-<tr><td align="center"><img src="http://tinypages.ie/logo.png"></td><td align="center"><img src="http://tinypages.ie/logo.png"></td><td align="center"><img src="http://tinypages.ie/logo.png"></td></tr>
-<tr><td align="center"><img src="http://tinypages.ie/logo.png"></td><td align="center"><img src="http://tinypages.ie/logo.png"></td><td align="center"><img src="http://tinypages.ie/logo.png"></td></tr>
-</table></center>
+<div class="coda-slider-wrapper">
+       <div class="coda-slider preload" id="coda-slider-1">
+               <div class="panel">
+                       <div class="panel-wrapper">
+                                <img src="http://tinypages.ie/logo.png" />                              
+                       </div>
+               </div>
+
+<div class="panel">
+                       <div class="panel-wrapper">
+                                      <img src="http://tinypages.ie/logo.png" />                         
+                       </div>
+               </div>
+
+<div class="panel">
+                       <div class="panel-wrapper">
+                                 <img src="http://tinypages.ie/logo.png" />                              
+                       </div>
+               </div>
+
+
+	</div><!-- .coda-slider -->
+</div><!-- .coda-slider-wrapper --></center>
 </div>
 </div>
 </body>
