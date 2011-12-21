54712dae994ff15a80a919ce3e83df80c0121601
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 15:24:00 +0000

    bug fixes

--- public/index.php
+++ public/index.php
@@ -28,6 +28,8 @@
 	
 		add_user($user_array);
 	}
+
+	print_r($cookie);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
