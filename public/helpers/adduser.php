4aab78a06341e4dc71dd3e1e046c67699e6027cd
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 18:01:24 +0000

    new

--- public/helpers/adduser.php
+++ public/helpers/adduser.php
@@ -1,7 +1,7 @@
 <?php
 	include('config.php');
 	
-	$username = $_POST['username'];
+	$user_name = $_POST['username'];
 	$password = $_POST['password'];
 
 	add_user($username, $password);
@@ -9,7 +9,7 @@
 	function add_user($username, $password){
 		global $mysqli; global $db_name;
 				
-		$insert_user = "insert into $db_name.users(username,password) values ($username, $password)";
+		$insert_user = "insert into $db_name.users (username,password) values ($user_name, $password)";
 		$result = $mysqli->query($insert_user) or die("Error Addind: ".$mysqli->error);
 		header("Location: http://deployment-project.orchestra.io/?s=1");
 	}
