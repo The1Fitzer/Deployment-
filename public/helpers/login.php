2af746c5f75449399c5dccf9e5da0210bc155ee5
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 19:52:54 +0000

    login functionality added

--- public/helpers/login.php
+++ public/helpers/login.php
@@ -17,11 +17,13 @@
 		$check2 = $result->fetch_object()->username;
 		
 		if(isset($check2)){
-			$id = $result->fetch_object()->id;
+			while($row = $result->fetch_object()){
+				$id = $row->id;
+			}	
 			$time = time() + 3600;
 			setcookie('username', $username, $time);
-			setcookie('id', $is, $time);
-			print_r($_COOKIES);
+			setcookie('id', $id, $time);
+			print_r($_COOKIE);
 			die();
 			header('Location: http://deployment-project.orchestra.io/?s=4');
 
