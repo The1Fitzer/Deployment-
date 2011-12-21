bc72211754ee4ec381d3d730ca2f90ddafabeb84
Gary Fitzpatrick <fityp12@gmail.com>
2011-12-21 19:58:47 +0000

    login functionality added

--- public/helpers/login.php
+++ public/helpers/login.php
@@ -20,7 +20,7 @@
 			while($row = $result->fetch_object()){
 				$id = $row->id;
 			}
-			echo "$id<br/><br/>"	
+			echo "$id<br/><br/>";	
 			$time = time() + 3600;
 			setcookie('username', $username, $time);
 			setcookie('id', $id, $time);
