7f42c95332984a0278864abc54f13907ca74d4f3
Gary Fitzpatrick <fityp12@gmail.com>
2012-01-02 19:25:52 +0000

    stuff shown on login

--- public/index.php
+++ public/index.php
@@ -75,7 +75,7 @@
 <div id="menu"><br /><br /><br /><br /><br /><br /><br /><a href="../"  class="links">Home</a><br /><a href="/videos"  class="links">Videos</a><br /><a href="../photos" class="links">Photos</a><br /><a href="../about" class="links">About NCI</a><br /><a href="../contact" class="links">Contact</a><br/>	
 	<?php 
 		if(isset($_COOKIE['ID_my_site'])){
-			echo "<a href='moodle.ncirl.ie' class='links'>Moodle</a>";
+			echo "<a href='http://moodle.ncirl.ie' class='links'>Moodle</a>";
 		}
 	?>
 	</div>
