b17db4872956fc9af110f5cd0f9b41b8804a13da
Gary Fitzpatrick <fityp12@gmail.com>
2012-01-03 11:59:19 +0000

    fix edit

--- public/index.php
+++ public/index.php
@@ -112,7 +112,6 @@
 							<table border=0>
 								<tr>
 							<?php 
-							$youtube = shuffle($youtube);
 							foreach($youtube as $val){ 
 										echo "<td><a class='video' href='http://www.youtube.com/v/$val?fs=1&amp;autoplay=1'><img id='$val' src='http://img.youtube.com/vi/$val/0.jpg' height='150px' width='200px'/></a></td>";
 									}
@@ -120,7 +119,6 @@
 								</tr>
 								<tr>
 									<?php 
-									$youtube2 = shuffle($youtube2);
 									foreach($youtube2 as $val2){ 
 												echo "<td><a class='video' href='http://www.youtube.com/v/$val2?fs=1&amp;autoplay=1'><img id='$val2' src='http://img.youtube.com/vi/$val2/0.jpg' height='150px' width='200px'/></a></td>";
 											}
@@ -175,7 +173,6 @@
 							<table border=0>
 								<tr>
 							<?php 
-							$youtube = shuffle($youtube);
 							foreach($youtube as $val){ 
 										echo "<td><a class='video' href='http://www.youtube.com/v/$val?fs=1&amp;autoplay=1'><img id='$val' src='http://img.youtube.com/vi/$val/0.jpg' height='150px' width='200px'/></a></td>";
 									}
@@ -183,7 +180,6 @@
 								</tr>
 								<tr>
 									<?php 
-									$youtube2 = shuffle($youtube2);
 									foreach($youtube2 as $val2){ 
 												echo "<td><a class='video' href='http://www.youtube.com/v/$val2?fs=1&amp;autoplay=1'><img id='$val2' src='http://img.youtube.com/vi/$val2/0.jpg' height='150px' width='200px'/></a></td>";
 											}
