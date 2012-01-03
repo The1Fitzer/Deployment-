5ac12df253ca2ec413a43e5709bcd3dad80cee0e
Gary Fitzpatrick <fityp12@gmail.com>
2012-01-03 11:56:28 +0000

    randomize array

--- public/index.php
+++ public/index.php
@@ -111,13 +111,17 @@
 					  <div class="panel-wrapper">
 							<table border=0>
 								<tr>
-							<?php foreach($youtube as $val){ 
+							<?php 
+							$youtube = shuffle($youtube);
+							foreach($youtube as $val){ 
 										echo "<td><a class='video' href='http://www.youtube.com/v/$val?fs=1&amp;autoplay=1'><img id='$val' src='http://img.youtube.com/vi/$val/0.jpg' height='150px' width='200px'/></a></td>";
 									}
 							?>
 								</tr>
 								<tr>
-									<?php foreach($youtube2 as $val2){ 
+									<?php 
+									$youtube2 = shuffle($youtube2);
+									foreach($youtube2 as $val2){ 
 												echo "<td><a class='video' href='http://www.youtube.com/v/$val2?fs=1&amp;autoplay=1'><img id='$val2' src='http://img.youtube.com/vi/$val2/0.jpg' height='150px' width='200px'/></a></td>";
 											}
 									?>
@@ -170,13 +174,17 @@
 					  <div class="panel-wrapper">
 							<table border=0>
 								<tr>
-							<?php foreach($youtube as $val){ 
+							<?php 
+							$youtube = shuffle($youtube);
+							foreach($youtube as $val){ 
 										echo "<td><a class='video' href='http://www.youtube.com/v/$val?fs=1&amp;autoplay=1'><img id='$val' src='http://img.youtube.com/vi/$val/0.jpg' height='150px' width='200px'/></a></td>";
 									}
 							?>
 								</tr>
 								<tr>
-									<?php foreach($youtube2 as $val2){ 
+									<?php 
+									$youtube2 = shuffle($youtube2);
+									foreach($youtube2 as $val2){ 
 												echo "<td><a class='video' href='http://www.youtube.com/v/$val2?fs=1&amp;autoplay=1'><img id='$val2' src='http://img.youtube.com/vi/$val2/0.jpg' height='150px' width='200px'/></a></td>";
 											}
 									?>
