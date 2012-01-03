bb3fb589a8a47bc5d238b31cf424130f7b2aa88a
Sam Cogan <cogansam@gmail.com>
2012-01-03 11:20:46 +0000

    about

--- public/about/index.php
+++ public/about/index.php
@@ -33,7 +33,50 @@
 			}
 		?></div>
 <div id="content">
-<h1> Welcome to NCI</h1>
+<h1>NCI History</h1>
+
+<p>National College of Ireland began its life in 1951, Ranelagh’s Stanford Lodge was
+became home to the Catholic Workers College which is what it was known as back
+then. There were 103 registered students in the first year and lectures were held by
+a small group of dedicated Jesuits two nights a week.</p>
+
+<p>10 years later student numbers increased, and by 1966 there were about 1300
+students. The college was re-branded National College of Industrial Relations
+(NCIR).</p>
+
+<p>In 1998 the college re-branded as the National College of Ireland with an extended
+National Campus Network and a multitude of national outreach programmes. The
+land and buildings at Sandford Road were transferred by the Jesuits to the NCI
+Board of management.</p>
+
+<p>The college took on a €25,000,000 fundraising campaign to relocate to a 2 acre site
+in the International Financial Services Centre, in Dublin. The College offers degree
+and diploma courses in areas including accountancy, community studies, computing
+and finance. There are about 5,000 students currently attending, full and part-time.
+The college has more than130 full-time and 220 part-time staff.</p>
+
+<h2>Life at NCI (National College of Ireland)</h2>
+
+<p>National College of Ireland promote smaller class sizes and lectures are highly
+approachable. This can help students to quickly feel at home in NCI and become
+part of campus life. The college restaurant is a great place to spend time with friends
+between classes or participate in events organized by the numerous clubs and
+societies.</p>
+
+<h2>Students Union</h2>
+
+<p>The Students Union or (SU) is the body that represents students of NCI. The
+services of the SU are available to full and part-time students. Students can become
+involved in the union and are encouraged to organize and influence events.</p>
+
+<p>Union Officers are elected by the students and are active for one academic year.
+The Students Union promotes all aspects of college life for students. Clubs and
+Societies are formed through the Students Union. The Union has representation
+on the College’s Governing Body and Academic Council. This makes it possible
+for students to have a say in the college. By becoming a student of NCI you also
+become a member of the Students Union. There Officers of the SU work together to
+provide education, welfare, equality, clubs and societies, entertainments and part-
+time support.</p>
 
 </div>
 </div>
